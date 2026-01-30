980567954a298ef46a764bdedc637786ba4600a1
cvgaalen <cvgaalen@quintor.nl>
2026-01-30 14:51:32 +0100

    Gallery page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Jochem } from './pages/jochem/jochem';
+import {Gallery} from './pages/gallery/gallery';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'jochem', component: Jochem },
+  { path: 'gallery', component: Gallery },
   { path: '**', redirectTo: '' }
 ];

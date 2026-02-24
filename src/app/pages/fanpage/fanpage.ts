443f11950af7e4a5ca46ab903a7997e3727dd326
cvgaalen <cvgaalen@quintor.nl>
2026-02-24 10:49:54 +0100

    Fan mail

--- src/app/pages/fanpage/fanpage.ts
+++ src/app/pages/fanpage/fanpage.ts
@@ -22,6 +22,20 @@ export class Fanpage {
       date: '2026-02-17',
       character: 'Jochem',
       content: 'Jochem is een echte Jochem.'
+    },
+    {
+      author: 'Anoniempje',
+      date: '2026-02-19',
+      character: 'Jochem',
+      content: 'Beste Jochem,\n' +
+        '\n' +
+        'Je bent een groot idool van mij, hoe je piano speelde en patat vrat. Ik hoop dat meer kinderen op de middelbare school zoals jij worden. En Ton Kas zat in je film, groot fan!'
+    },
+    {
+      author: 'Anonieme Jochem fan',
+      date: '2026-02-24',
+      character: 'Jochem',
+      content: 'Spijt heeft mijn leven veranderd. Elke keer wanneer ik in mijn dagelijks leven het woord "spijt" hoor, word ik nu getriggerd. Ik ga mijn kind later Jochem noemen denk ik.'
     }
   ];
 
